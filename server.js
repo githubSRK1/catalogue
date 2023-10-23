60d52529b747ef02c5d5ceb24b149f85a6d84ec1
SRK <RanjithSrikonda.Devops@example.com>
2023-10-23 14:11:59 +0530

    new version

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OKKK',
+        app: 'OKKKKK',
         mongo: mongoConnected
     };
     res.json(stat);
